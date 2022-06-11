41cb66521951707e7e1c58e7f3300e9be19a3d0c
CodingRafi <muhamadrafi1408@gmail.com>
2022-06-11 20:22:53 +0700

    finish

---
+++ js/script.js
@@ -0,0 +1,13 @@
+gsap.from('.navbar', {
+    duration: 2,
+    y: -100,
+    opacity: 0,
+    ease: 'bounce'
+});
+
+gsap.from('.contact', {
+    duration: 2,
+    y: -100,
+    opacity: 0,
+    ease: 'bounce'
+});
